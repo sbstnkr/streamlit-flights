416b7c8399875ac4c0e409e5a6707367d8d56f25
sbstnkr <sebastian.krawczyk1116@gmail.com>
2022-09-30 13:22:06 +0200

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,15 @@ import streamlit as st
 from google.cloud import firestore
 from google.oauth2 import service_account
 import json
+import pandas as pd
 
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds)
 
-flights = {doc.id: doc.to_dict() for doc in db.collection("flights").stream()}
+flights = [{'date': doc.id,
+            'data': doc.to_dict()} for doc in db.collection("flights").stream()]
+
+print(pd.DataFrame.from_records(flights))
 
 st.line_chart(data=flights)
