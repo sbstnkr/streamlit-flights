71ad3fd22bc5836d2ec238460ce51f485c9e5c2c
sbstnkr <sebastian.krawczyk1116@gmail.com>
2022-09-30 12:29:27 +0200

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,14 @@
-import streamlit
+import streamlit as st
+from google.cloud import firestore
+from google.oauth2 import service_account
+import json
 
+key_dict = json.loads(st.secrets["textkey"])
+creds = service_account.Credentials.from_service_account_info(key_dict)
+db = firestore.Client(credentials=creds)
+
+posts_ref = db.collection("flights")
+
+for doc in posts_ref.stream():
+    st.write("The id is: ", doc.id)
+    st.write("The contents are: ", doc.to_dict())
