9c1bfaeddbdfdd43365bf3f73dc68fc32c977df5
sbstnkr <sebastian.krawczyk1116@gmail.com>
2022-09-30 14:19:17 +0200

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,10 +20,47 @@ df = df[['date', 'city', 'price']]
 
 print(df)
 
-c = alt.Chart(df).mark_line().encode(
-    x='date', y='price', size='city', color='city', tooltip=['price', 'city', 'date']
-)
+def get_chart(data):
+    hover = alt.selection_single(
+        fields=["date"],
+        nearest=True,
+        on="mouseover",
+        empty="none",
+    )
 
-st.altair_chart(c, use_container_width=True)
+    lines = (
+        alt.Chart(data, title="Flights")
+        .mark_line()
+        .encode(
+            x="date",
+            y="price",
+            color="city",
+        )
+    )
+
+    # Draw points on the line, and highlight based on selection
+    points = lines.transform_filter(hover).mark_circle(size=65)
+
+    # Draw a rule at the location of the selection
+    tooltips = (
+        alt.Chart(data)
+        .mark_rule()
+        .encode(
+            x="date",
+            y="price",
+            opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
+            tooltip=[
+                alt.Tooltip("date", title="Date"),
+                alt.Tooltip("price", title="Price (PLN)"),
+            ],
+        )
+        .add_selection(hover)
+    )
+    return (lines + points + tooltips).interactive()
+
+
+chart = get_chart(df)
+
+st.altair_chart(chart, use_container_width=True)
 
 #st.line_chart(data=df, x='date', y='price')
